831229961ce0e10ad256eeae3eef1281b58b9593
mjcrntx <marjoycaranto29@gmail.com>
2024-10-27 02:36:35 +0800

    Pseudocode

---
+++ range_number_counter.py
@@ -0,0 +1,12 @@
+# Given range:
+# 1-10 = ?
+# 11-20 = ?
+# 21-30 = ?
+# 31-40 = ?
+# 41-50 = ?
+
+#Use array to store the user's number
+
+#Use loop to ask the user to input their number from 1-50
+
+#Print the result of how many inputted numbers are there in the given range
