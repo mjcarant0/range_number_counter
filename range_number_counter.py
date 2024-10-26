3e68b34d376c1938168ebe9faba04151a726124c
mjcrntx <marjoycaranto29@gmail.com>
2024-10-27 02:47:55 +0800

    Using array to store how many inputted numbers are there in every range and using if statements to add 1 in a value in every input

--- range_number_counter.py
+++ range_number_counter.py
@@ -6,7 +6,29 @@
 # 41-50 = ?
 
 #Use array to store the user's number
+range_number = {
+    "1-10": 0,
+    "11-20": 0,
+    "21-30": 0,
+    "31-40": 0,
+    "41-50": 0
+}
 
 #Use loop to ask the user to input their number from 1-50
-
+while True:
+    try:
+        user_number = int(input("Choose a number from 1-50: "))
+        
+        if 1 <= user_number >= 10:
+            range_number["1-10"] += 1
+        if 11 <= user_number >= 20:
+            range_number["11-20"] += 1
+        if 21 <= user_number >= 30:
+            range_number["21-30"] += 1
+        if 31 <= user_number >= 40:
+            range_number["31-40"] += 1
+        if 41 <= user_number >= 50:
+            range_number["41-50"] += 1
+    except:
+        break
 #Print the result of how many inputted numbers are there in the given range
